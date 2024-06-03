eeb441d793b9b65f755565d7544fc94e4b594716
Eng-Liban <LibanAbdulahi0008@gmail.com>
2024-06-03 18:16:24 +0300

    file

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -2,11 +2,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import {BrowserRouter} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "./index.css"
 const container = document.querySelector("#root");
 
-// export let endpoint='https://student-authorization.onrender.com/api'
+export let endpoint='https://student-authorization.onrender.com/api'
 
-export let endpoint='http://localhost:3000/api'
+// export let endpoint='http://localhost:3000/api'
 
 const root = createRoot(container);
 root.render(
